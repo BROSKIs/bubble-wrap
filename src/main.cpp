7e73ab085ec8d4455f8f64aeb1de9bfb6c7ebf5a
Pasha <pavloyousee@gmail.com>
2026-01-29 02:13:49 +0000

    Bubble Wrap Sprite Transformations

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ int main()
 {
     bn::core::init();
 
-    bn::vector<bn::sprite_ptr, 300> circles = {};
+    bn::vector<bn::sprite_ptr, 90> circles = {};
     for (int y = -40; y <= 40; y += 10)
     {
         for (int x = -40; x <= 40; x += 10)
@@ -18,6 +18,29 @@ int main()
             circles.push_back(bn::sprite_items::dot.create_sprite(x, y));
         }
     }
+
+    for (int i = 0; i < circles.size(); i++)
+    {
+        if (i == 0 || i == 8 || i == 72 || i == 80)
+        {
+            circles[i].set_scale(1.8);
+        }
+        if (i < 8 && i > 0 || i < 80 && i > 72)
+        {
+            circles[i].set_vertical_scale(0.6);
+            circles[i].set_horizontal_scale(1.5);
+        }
+        if (((i - 8) % 9 == 0 || i % 9 == 0) && i != 0 && i != 72 && i != 8 && i != 80)
+        {
+            circles[i].set_vertical_scale(1.5);
+            circles[i].set_horizontal_scale(0.6);
+        }
+    }
+
+    // circles[0].set_scale(1.8);
+    // circles[1].set_scale(0.5);
+    // circles[8].set_scale(1.8);
+    // circles[9].set_horizontal_scale(0.6);
 
     bn::backdrop::set_color(bn::color(20, 20, 31));
 
